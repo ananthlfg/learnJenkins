7d09737159230e3c6865eac6609c7c9f983a6b7f
ananthlfg <tn.ananth1505@gmail.com>
2019-05-18 18:28:52 -0400

    added jenkinsdemo prog

---
+++ src/printNum/printNumbers.java
@@ -0,0 +1,17 @@
+package printNum;
+
+public class printNumbers {
+
+
+
+			public static void main(String[] args) {
+				
+				for(int i=0;i<=10;i++){
+					System.out.println("happy learning :"+i);
+				}
+
+			}
+
+
+
+}
