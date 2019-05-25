0f05c56bff430abe9cd2329d124fe536ff9aa0a7
ananthlfg <tn.ananth1505@gmail.com>
2019-05-24 23:09:18 -0400

    seventh change

--- src/printNum/printNumbers.java
+++ src/printNum/printNumbers.java
@@ -6,7 +6,7 @@ public class printNumbers {
 
 			public static void main(String[] args) {
 				
-				for(int i=0;i<=40;i++){
+				for(int i=0;i<=45;i++){
 					System.out.println("happy learning :"+i);
 				}
 
